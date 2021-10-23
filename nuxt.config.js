19b9092bce06472f7dc290051c4360ed4e174959
rokealvo <rokealvo@gmail.com>
2021-10-23 21:23:05 +0700

    added mintUI

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,9 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: [
+    'mint-ui/lib/style.css'
+  ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,35 @@
 <template>
   <div>
     <h1>Очень нужное миру приложение</h1>
+    <p>input value: <span>{{ input }}</span></p>
+    <Field
+      v-model="input"
+      placeholder="placeholder"
+    />
+    <Button
+      type="primary"
+      icon="more"
+      @click="onClick"
+    >
+      button
+    </Button>
   </div>
 </template>
 
 <script>
+import {Button, Field, Toast} from 'mint-ui';
+
 export default {
+  components: {Button, Field},
+  data() {
+    return {
+      input: ''
+    }
+  },
+  methods: {
+    onClick() {
+      Toast('button clicked')
+    },
+  }
 }
 </script>
