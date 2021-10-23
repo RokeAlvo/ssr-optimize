71c54bb640f31953478a7b0cecfdb9c2b73274d0
rokealvo <rokealvo@gmail.com>
2021-10-23 15:16:32 +0700

    test without bulmUI

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,12 +18,12 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
-    'balm-ui/dist/balm-ui.css'
+    // 'balm-ui/dist/balm-ui.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '@/plugins/balm-ui'
+    // '@/plugins/balm-ui'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
