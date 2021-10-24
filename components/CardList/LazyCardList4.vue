a1a849addfd37831141009f440907fc9f76296e3
rokealvo <rokealvo@gmail.com>
2021-10-24 21:39:55 +0700

    add lazyHydration and dynamic imports

--- components/CardList/LazyCardList4.vue
+++ components/CardList/LazyCardList4.vue
@@ -7,7 +7,7 @@ const CardList4 = () =>
   import(/* webpackChunkName: "CardList4" */ '~/components/CardList/CardList4.vue')
 
 export default {
-  name: "LazyCardList14",
+  name: "LazyCardList4",
   components: {CardList4},
   props: {
     productsGroup: {
