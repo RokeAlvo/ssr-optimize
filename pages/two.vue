eac4251e875aa9d409a64303bfba841f6ffc2c17
Nikolay Koshkarev <nikolai.koshkarev@xiag.ch>
2022-04-20 16:21:39 +0700

    add page2

---
+++ pages/two.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <h1>Page two</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
+<script>
+
+export default {
+  name: 'TwoPage',
+}
+</script>
+<style>
+.main-image {
+  width: 375px;
+  height: 375px;
+}
+</style>
